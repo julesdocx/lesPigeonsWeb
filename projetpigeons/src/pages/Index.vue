aa859ce185ba214ec533758a22fbc9a1d5ae202d
Dmitriy Kozlov <dimitry_kozlov@hotmail.com>
2021-08-29 20:40:05 +0200

    adding mixins+adding part nav

--- projetpigeons/src/pages/Index.vue
+++ projetpigeons/src/pages/Index.vue
@@ -1,24 +1,35 @@
 <template>
-<header class="header">
-      <nav class="nav">
-        <g-link class="nav__link" to="/about">Over ons</g-link>
-        <g-link class="nav__link" to="/contact">Contact</g-link>
-        <g-link class="nav__link" to="/manifest">Manifest</g-link>
-        <g-link class="nav__link" to="/nieuws">Nieuws</g-link>
+  <div class="homepage">
+    <header class="homepage_header">
+      <nav class="homepage_header_nav">
+        <g-link class="homepage_header_nav--nav__link" to="/about"
+          >Over ons</g-link
+        >
+        <g-link class="homepage_header_nav--nav__link" to="/contact"
+          >Contact</g-link
+        >
       </nav>
- 
-    <g-image alt="logo" src="~/LOgoBLACK.png" width="135" />
-   </header>
-
-</template> 
+      <div class="img">
+        <g-image alt="logo" src="~/LOgoBLACK.png" />
+      </div>
+      <nav class="homepage_header_nav">
+        <g-link class="homepage_header_nav--nav__link" to="/manifest"
+          >Manifest</g-link
+        >
+        <g-link class="homepage_header_nav--nav__link" to="/nieuws"
+          >Nieuws</g-link
+        >
+      </nav>
+    </header>
+  </div>
+</template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'HomePage'
-  }
-}
+    title: "HomePage",
+  },
+};
 </script>
 
-<style>
-</style>
+<style lang="scss"></style>
